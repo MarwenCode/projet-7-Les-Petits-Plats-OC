303638babaef208f92580dabe5153a08ed11d77e
MarwenCode <marwen_jdidi@yahoo.com>
2023-12-25 23:52:12 +0100

    starter code

---
+++ index.js
@@ -0,0 +1,66 @@
+import recipes from "./data/recipes.js";
+
+// create cartes
+const cartes = document.querySelector(".cartes");
+
+const displayCartes = (recipes) => {
+  for (let i = 0; i < recipes.length; i++) {
+    let recipe = recipes[i];
+
+    let carte = document.createElement("article");
+    carte.classList.add("carte");
+
+    // Assuming 'ingredients' is an array in each recipe
+    let listeIngredients = "";
+    for (let j = 0; j < recipe.ingredients.length; j++) {
+      const ingredient = recipe.ingredients[j];
+      // Check if quantity and unit are present, otherwise, display an empty string
+      const quantity = ingredient.quantity
+        ? `<p>${ingredient.quantity}</p>`
+        : "";
+      const unit = ingredient.unit ? `<span>${ingredient.unit}</span>` : "";
+      listeIngredients += 
+      `<div class="ingredients-info">
+      <div class="title">
+      <p>${ingredient.ingredient}</p>
+      </div>
+      <div class="unit">
+      <p>${quantity}</p>
+    <span>${unit}</span>
+  </div>
+
+
+    </div>`;
+    }
+    carte.innerHTML = `
+      <div class="carte_image">
+        <img
+          src="photos/${recipe.image}"
+          class="card-img-top"
+          alt="Illustration plat"
+        />
+      </div>
+      <div class="title">
+        <h2>${recipe.name}</h2>
+      </div>
+      <div class="time">
+        <p>${recipe.time} min</p>
+      </div>
+      <div class="description-section">
+        <p class="description">${recipe.description}</p>
+      </div>
+      <div class="ingredients-section">
+        <p>Ingrédients</p>
+        <div class="ingredients">
+          ${listeIngredients}
+        </div>
+      </div>
+    `;
+
+    cartes.append(carte);
+  }
+};
+
+displayCartes(recipes);
+
+console.log(recipes)
